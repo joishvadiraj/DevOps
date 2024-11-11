9492d9158718199ace3ceb590b488c3282567bea
git config --global user.name joishvadiraj <joish.vadiraj@gmail.com>
2024-11-11 17:16:15 +0100

    python files

---
+++ Python/DataTypes/datatypes_1.py
@@ -0,0 +1,12 @@
+#
+#Python has several basic data types:
+#Integer (int): Whole numbers like 5 or -10
+#Float (float): Decimal numbers like 3.14 or 0.001
+#String (str): Text, written within quotes, like "Hello"
+#Boolean (bool): True or False
+
+name = "Web Developer"
+age = 50
+height = 6
+
+print (name, age, height)
---
+++ Python/DataTypes/datatypes_2
@@ -0,0 +1,6 @@
+Name = "Devops Engineer"
+Age = 80
+City = "Prague"
+Height = 5.7
+
+print(Name, Age, City, Height)
