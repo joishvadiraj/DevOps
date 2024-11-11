66d280eebe03ebd55c791863b0eeefa6368a7923
git config --global user.name joishvadiraj <joish.vadiraj@gmail.com>
2024-11-11 20:17:28 +0100

    new py files

---
+++ Python/DataTypes/datatypes_3.py
@@ -0,0 +1,11 @@
+#Basic Operations
+#Python supports basic arithmetic operations: +, -, *, /, and % (modulus).
+
+a = 10
+b = 11
+
+print("Addition =", a + b)
+print("Substraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
